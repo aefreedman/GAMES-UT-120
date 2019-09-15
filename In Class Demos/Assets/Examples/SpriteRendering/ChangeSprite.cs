f63eaa34b9ea0f32c379c0b42a82f6aee35beaed
aefreedman <aaron@secretcrush.net>
2019-09-15 18:25:30 -0400

    sprite rendering example folder

---
+++ In Class Demos/Assets/Examples/SpriteRendering/ChangeSprite.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class ChangeSprite : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer Renderer;
+
+    [SerializeField]
+    private Sprite[] Sprites;
+
+    private int _index;
+
+    private void Start()
+    {
+        if (Renderer == null)
+            Renderer = GetComponent<SpriteRenderer>();
+
+        SetSprite(0);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            NextSprite();
+    }
+
+    private void SetSprite(int toSprite)
+    {
+        if (toSprite > Sprites.Length - 1)
+            toSprite = Sprites.Length - 1;
+        
+        _index = toSprite;
+        Renderer.sprite = Sprites[_index];
+    }
+
+    private void NextSprite()
+    {
+        _index++;
+        if (_index >= Sprites.Length)
+            _index = 0;
+        
+        SetSprite(_index);
+    }
+}
